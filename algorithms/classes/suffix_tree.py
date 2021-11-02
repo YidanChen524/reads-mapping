c353cad802d1bc58afa6282ee8cfa8d85beb0735
YidanChen524 <yidanchen524@gmail.com>
2021-11-02 15:32:55 +0100

    finished search method in suffix tree

--- algorithms/classes/suffix_tree.py
+++ algorithms/classes/suffix_tree.py
@@ -1,17 +1,20 @@
 class SuffixTree:
     class Node:
-        def __init__(self, start, end):
+        def __init__(self, start, end, leaf=None):
             self.start = start
             self.end = end
             self.next = {}
+            self.leaves = []
+            if leaf is not None:
+                self.leaves.append(leaf)
 
     def __init__(self, x):
         self.x = x + "$"
         self.len = len(self.x)
-        self.root = self.Node(0, 0)
+        self.root = self.Node(0, self.len, 0)
         # construct the suffix tree by adding each suffix one by one starts from position 1
         for i in range(1, self.len):
-            self.root = self.match(i, self.root)
+            self.root = self.add_suffix(i, self.root, i)
 
     def __str__(self, level=0, node=None):
         """print the suffix tree"""
@@ -20,12 +23,14 @@ class SuffixTree:
             node = self.root
         else:
             ret = " " + "      " * (level-1) + "|__: \""
-        ret += self.x[node.start:node.end] + "\"\n"
+        ret += self.x[node.start:node.end] + "\""
+        ret += f" {node.leaves}"
+        ret += "\n"
         for _, child in node.next.items():
             ret += self.__str__(level+1, child)
         return ret
 
-    def match(self, i, current):
+    def add_suffix(self, i, current, leaf):
         """
         match suffix start from index i to the label stored in current node
         if there is a mismatch or reaching the end, create a new node
@@ -36,38 +41,43 @@ class SuffixTree:
                 new_node = self.Node(current.start, current.start+j)
                 current.start = current.start + j
                 new_node.next[self.x[current.start]] = current
-                new_node.next[self.x[i+j]] = self.Node(i+j, self.len)
+                new_node.next[self.x[i+j]] = self.Node(i+j, self.len, leaf)
+                new_node.leaves += [*current.leaves, leaf]
                 return new_node
         else:
             if self.x[i+m] in current.next:
-                current.next[self.x[i+m]] = self.match(i+m, current.next[self.x[i+m]])
+                current.next[self.x[i+m]] = self.add_suffix(i + m, current.next[self.x[i + m]], leaf)
+                current.leaves.append(leaf)
                 return current
             else:
-                current.next[self.x[i+m]] = self.Node(i+m, self.len)
+                current.next[self.x[i+m]] = self.Node(i+m, self.len, leaf)
+                current.leaves.append(leaf)
                 return current
 
-    def find(self, p, current):
-        j = 0
-        if len(p) == 0 or current is None:
-            return [-1]
-        for i in range(current.end - current.start):
-            if i == len(p):
-                return [current.start + i - len(p) + 1]
-            if self.x[current.start + i] != p[i]:
-                return [-1]
+    def find_recursive(self, p, p_ind, current):
+        """
+        if p finds a match in the current node, return the position
+        if there is a mismatch, return -1
+        else continue the search in the next node
+        """
+        m, n = len(p) - p_ind, current.end - current.start
+        for i in range(min(m, n)):
+            if p[p_ind+i] != self.x[current.start+i]:
+                return -1
         else:
-            p = p[current.end:]
-            if len(p) == 0:
-                pos = []
-                for node in current.next.values():
-                    pos.append(node.start - len(p))
-                return pos
-            if p[0] not in current.next:
-                return [-1]
-            current = current.next[p[0]]
-            return self.find(p, current)
+            if m <= n:
+                return current.leaves
+            else:
+                if p[p_ind+n] in current.next:
+                    return self.find_recursive(p, p_ind+n, current.next[p[p_ind+n]])
+                else:
+                    return -1
+
+    def find(self, p):
+        return self.find_recursive(p, 0, self.root)
 
 
 if __name__ == "__main__":
-    st = SuffixTree("hello")
+    st = SuffixTree("mississippi")
     print(st)
+    print(st.find("ssippi"))
--- algorithms/search-st.py
+++ algorithms/search-st.py
@@ -7,10 +7,10 @@ def search_st(refs, reads):
     for ref_key, ref_val in refs.items():
         st = SuffixTree(ref_val)
         for read_key, read_val in reads.items():
-            positions = st.find(read_val, st.root)
-            if positions[0] != -1:
+            positions = st.find(read_val)
+            if positions != -1:
                 for pos in positions:
-                    helpers.print_sam(QNAME=read_key, RNAME=ref_key, POS=pos,
+                    helpers.print_sam(QNAME=read_key, RNAME=ref_key, POS=pos+1,
                                       CIGAR=f'{str(len(read_val))}M', SEQ=read_val, QUAL='~'*len(read_val))
 
 
@@ -19,5 +19,4 @@ if __name__ == '__main__':
     refs = helpers.parse_fa(sys.argv[1])
     reads = helpers.parse_fq(sys.argv[2])
     # run the naive st search algorithm and print the output in SAM format
-    # search_st(refs, reads)
-    search_st({'1': "hello"}, {'1': "l"})
+    search_st(refs, reads)
